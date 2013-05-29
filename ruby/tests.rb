cf3a941793ccb1fbac9cc5827ffe615a71305d33
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-05-28 23:35:06 -0500

    added external file for ruby

--- ruby/tests.rb
+++ ruby/tests.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 require 'minitest/spec'
 require 'minitest/autorun'
+require_relative 'main'
 
 
-describe 'Tests' do
+describe 'Test Something' do
 
-    it 'should fail' do
-        assert false, "Write a Test"
+  it 'should fail' do
+    assert_equal false, something
+  end
 
-    end
+  it 'should pass' do
+    assert_equal true, something
+  end
 
 end
