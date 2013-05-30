6fd5c65dc48b7fd0f766324e232e3d4568ecfab8
Percy <toran.billups@theimtgroup.com>
2013-05-30 15:06:35 -0500

    kata

--- python/main.py
+++ python/main.py
@@ -1,3 +1,29 @@
+numerals = {
+    1:'I',
+    5:'V',
+    10:'X'
+}
 
-def something():
-    return True
+def PIRVal(nums,upper):
+    prev = False
+    nums.sort()
+    for num in nums:
+        if num > upper:
+            return prev
+        else:
+            prev = num
+    return prev
+
+def roman(i):
+    if numerals.has_key(i):
+        return numerals[i]
+    elif i < 4:
+        return 'I' * i
+    elif i == 4:
+        return 'IV'
+    elif i > 5 and i < 9:
+        return 'V' + roman(i - 5)
+    elif i == 9:
+        return 'IX'
+    elif i == 11:
+        return 'XI'
--- python/tests.py
+++ python/tests.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python
 
 import unittest
-from main import something
+from main import roman
+from main import PIRVal
 
+class TestPIRVal(unittest.TestCase):
+    def test_one_returned_when_given_1_and_2_with_max_of_1(self):
+        self.assertEqual(1,PIRVal([1,2], 1))
 
-class TestSomething(unittest.TestCase):
+    def test_2_returned_when_given_1_and_2_with_max_of_2(self):
+        self.assertEqual(2,PIRVal([1,2], 2))
 
-    def test_fail(self):
-        self.assertFalse(something())
+    def test_5_returned_when_given_1_and_5_and_10_with_max_of_7(self):
+        self.assertEqual(5,PIRVal([1,5,10], 7))
 
-    def test_pass(self):
-        self.assertTrue(something())
+class TestRoman(unittest.TestCase):
 
+    def test_1(self):
+        self.assertEqual("I", roman(1))
+
+    def test_2(self):
+        self.assertEqual("II",roman(2))
+
+    def test_3(self):
+        self.assertEqual("III",roman(3))
+
+    def test_4(self):
+        self.assertEqual("IV",roman(4))
+
+    def test_5(self):
+        self.assertEqual("V",roman(5))
+
+    def test_6(self):
+        self.assertEqual("VI",roman(6))
+
+    def test_7(self):
+        self.assertEqual("VII",roman(7))
+
+    def test_8(self):
+        self.assertEqual("VIII",roman(8))
+
+    def test_9(self):
+        self.assertEqual("IX",roman(9))
+
+    def test_10(self):
+        self.assertEqual("X",roman(10))
+
+    def test_11(self):
+        self.assertEqual("XI",roman(11))
 
 if __name__ == '__main__':
     unittest.main()
